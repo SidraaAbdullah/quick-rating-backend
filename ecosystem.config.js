ab376692c4c3ae11ea5c26e005994d58ea5459d1
Sarmed <sarmedrizvi@gmail.com>
2021-07-05 01:02:28 +0500

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'Quick Rating',
+      name: 'quick-rating',
       script: './src/server.js',
       env: {
         PORT: 8080,
         NODE_ENV: 'staging',
         MONGODB_URL:
           'mongodb+srv://quickrating:@DeveloperQuickRating@cluster0.be1wm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-        CACHE_TIME: 120,
+        CACHE_TIME: 180,
         CAN_VOTE_ALWAYS: false,
         PICTURES: true,
       },
